18f894718b14f609d4d0dcd7a58ca6836a1a4a89
Gerard Ribas Canals <gerard.ribas.canals@gmail.com>
2015-09-15 20:12:32 +0100

    Add Swagger2 for Rest Documentation

--- spring-mvc-rest/src/main/java/cat/grc/spring/mvc/rest/Application.java
+++ spring-mvc-rest/src/main/java/cat/grc/spring/mvc/rest/Application.java
@@ -7,11 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * @author Gerard Ribas (gerard.ribas.canals@gmail.com)
  *
  */
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan("cat.grc.spring")
 public class Application {
 
